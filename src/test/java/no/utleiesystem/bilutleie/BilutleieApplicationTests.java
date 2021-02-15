a83199c6d5d13d1b9eed24f27ca6b79688fbe4b6
PaTrond <trond@hauge.app>
2021-02-15 15:17:55 +0100

    init

---
+++ src/test/java/no/utleiesystem/bilutleie/BilutleieApplicationTests.java
@@ -0,0 +1,13 @@
+package no.utleiesystem.bilutleie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilutleieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
