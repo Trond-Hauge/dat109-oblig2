80113ec6505cc7aa170c749b924129cbf0937fc0
Cathrine Monsen <monsen.cathrine@gmail.com>
2021-02-16 11:54:31 +0100

    init

--- src/main/java/no/utleiesystem/bilutleie/Bil.java
+++ src/main/java/no/utleiesystem/bilutleie/Bil.java
@@ -3,5 +3,18 @@ package no.utleiesystem.bilutleie;
 public class Bil {
 
     private String regNummer; 
+
+    private String merke; 
+
+    private String farge; 
+
+    private String modell;
+
+    private boolean ledig; 
+
+    private char utleiegruppe; 
+
+
+
     
 }
