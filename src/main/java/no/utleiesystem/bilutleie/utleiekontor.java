59685e51dfffa6e5af4eec859d14fa97848e9976
PaTrond <trond@hauge.app>
2021-02-16 03:05:45 +0100

    korrigert klassenavn og opprettet JPA dependency

--- src/main/java/no/utleiesystem/bilutleie/utleiekontor.java
+++ src/main/java/no/utleiesystem/bilutleie/utleiekontor.java
@@ -1,5 +1,15 @@
 package no.utleiesystem.bilutleie;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Utleiekontor {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int nummer;
     
 }
