b58ad186c8f906af8c53f7646deb4bc87018cd3c
Cathrine Monsen <monsen.cathrine@gmail.com>
2021-02-16 16:11:36 +0100

    init

---
+++ src/main/resources/data.sql
@@ -0,0 +1,39 @@
+CREATE TABLE bil (
+
+        regNummer varchar(7),
+        merke VARCHAR(15),
+        farge VARCHAR (15),
+        modell VARCHAR (15),
+        ledig BOOL,
+        utleiegruppe CHAR (1),
+        kilometerstand INTEGER (100),
+
+        PRIMARY KEY (regNummer),
+        FOREIGN KEY (???) REFERENCES Utleiekontor(???) 
+);
+
+CREATE TABLE utleiekontor (
+
+        nummer INTEGER (15),
+        postnummer INTEGER (15),
+        gateadresse VARCHAR (15),
+        postnummer VARCHAR (15),
+        poststed VARCHAR (15),
+        tlf INTEGER (9)
+        PRIMARY KEY (nummer)
+);
+
+
+CREATE TABLE Kunde (
+
+        tlf CHAR (9),
+        fornavn VARCHAR (15),
+        etternavn VARCHAR (15),
+        kredittkort INTEGER (16),
+        PRIMARY KEY (tlf)
+);
+
+
+
+ 
+   
