db2981fab698809aa059fc3d68fff917792fddbb
PaTrond <trond@hauge.app>
2021-02-18 15:59:07 +0100

    lagt til import

--- src/main/java/no/utleiesystem/bilutleie/BilutleieApplication.java
+++ src/main/java/no/utleiesystem/bilutleie/BilutleieApplication.java
@@ -1,8 +1,12 @@
 package no.utleiesystem.bilutleie;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
 
+//@SpringBootApplication
 @Controller
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "no.utleiesystem.bilutleie.controller" )
