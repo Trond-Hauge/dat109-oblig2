0c8f97724618842451e1e13982e02a9d22cc313d
Cathrine Monsen <monsen.cathrine@gmail.com>
2021-02-18 21:43:51 +0100

    init

--- src/main/java/no/utleiesystem/bilutleie/BilutleieApplication.java
+++ src/main/java/no/utleiesystem/bilutleie/BilutleieApplication.java
@@ -1,17 +1,10 @@
 package no.utleiesystem.bilutleie;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Controller;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
-@Controller
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "no.utleiesystem.bilutleie.controller" )
+@SpringBootApplication
 public class BilutleieApplication {
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(BilutleieApplication.class, args);
